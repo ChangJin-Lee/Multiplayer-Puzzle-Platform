02cb614fa1e4b084c515fde02b5bb6e310006cac
ChangJin-Lee <ckdwls525@gmail.com>
2024-05-23 22:02:58 +0900

    HasAuthority(), SetActorLocation(), 생성자

---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMovingPlatform::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	MoveStartToEnd(DeltaSeconds);
+}
+
+
+void AMovingPlatform::MoveStartToEnd(float DeltaSeconds)
+{
+	if(HasAuthority())
+	{
+		SetActorLocation(GetActorLocation() + FVector(1,0,0) * Lambda * DeltaSeconds);
+	}
+}
+
